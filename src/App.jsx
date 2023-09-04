aa7ce18b3a787a75efe32ca9d0810fa50b4131fd
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-09-04 20:29:54 +0300

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,11 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
+       
+       
+       
         <Route index element={<Home />} />
-        
+
       </Route>
     </Routes>
   );
