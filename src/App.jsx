a2266dbce6c10c2de9e3364ff307379514cfb12a
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-09-06 16:25:57 +0300

    header + context + routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-
 import { Route, Routes } from "react-router-dom";
 import { SharedLayout } from "./SharedLayout/SharedLayout";
-import { Home } from "./modules/Home/Home";
+import { Home } from "./pages/Home";
+import { News } from "./pages/News";
+import { OurActivity } from "./pages/OurActivity";
+import { About } from "./pages/About";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
-
+        <Route path="news" element={<News />} />
+        <Route path="our-activity" element={<OurActivity />} />
+        <Route path="about-us" element={<About />} />
       </Route>
     </Routes>
   );
--- src/modules/Header/Header.jsx
+++ src/modules/Header/Header.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import style from "./Header.module.scss";
+import sprite from "../../assets/svg/sprite.svg";
+import { Navigation } from "../shared/Navigation/Navigation";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={style.header}>
+      <div className={style.logoRapper}>
+        <svg className={style.logo} width="42" height="42">
+          <use xlinkHref={`${sprite}#logo`}></use>
+        </svg>
+      </div>
+      <nav className={style.navMenu}>
+        <Navigation />
+      </nav>
+      <button className={style.button}>Підтримати</button>
+      <button className={style.burgerBtn}>
+        <svg className={style.menuBtn} width="24" height="24">
+          <use xlinkHref={`${sprite}#menu`}></use>
+        </svg>
+      </button>
+    </header>
+  );
 };
