6ec80cedfccfebee83bd6f8db304d982709c132e
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-09-08 02:26:51 +0300

    swiper 1st

--- src/modules/Home/OurAchievements/OurAchievements.jsx
+++ src/modules/Home/OurAchievements/OurAchievements.jsx
@@ -1,13 +1,91 @@
-import React from 'react'
-import style from "./OurAchievements.module.scss"
-const OurAchievements = () => {
-  return (
-		<div className={style.mainContainer}>
-		  <div className={style.container}>
-  			<h3 className={style.title}>Наші досягнення</h3>
-  		</div>
-		</div>
-	);
-}
+import React from "react";
+import style from "./OurAchievements.module.scss";
+import { Container } from "../../shared/Container/Container";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-export default OurAchievements
+import "swiper/css";
+import "swiper/css/pagination";
+import "../../../index.scss";
+import { Pagination } from "swiper/modules";
+
+const OurAchievements = () => {
+	return (
+		<Container>
+			<div className={style.container}>
+				<h3 className={style.title}>Наші досягнення</h3>
+
+				<>
+					<Swiper
+						pagination={true}
+						modules={[Pagination]}
+						className='mySwiper'>
+						<SwiperSlide>
+							{" "}
+							<div className={style.newContainer}>
+								<div className={style.textWrapper}>
+									<p className={style.text}>
+										Увійшли до книги рекордів України, як організація, яка
+										допомогла відзначити державними нагородами за один захід
+										найбільшу кількість матерів загиблих військових (350 батьків
+										Героїв отримали відзнаку «Знак пошани» та іменну «Подяку»,і
+										100 матерів медалі «Честь.Слава.Держава» )
+									</p>
+								</div>
+							</div>
+						</SwiperSlide>
+						<SwiperSlide>
+							{" "}
+							<div className={style.textWrapper}>
+								<p className={style.text}>
+									Увійшли до книги рекордів України, як організація, яка
+									допомогла відзначити державними нагородами за один захід
+									найбільшу кількість матерів загиблих військових (350 батьків
+									Героїв отримали відзнаку «Знак пошани» та іменну «Подяку»,і
+									100 матерів медалі «Честь.Слава.Держава» )
+								</p>
+							</div>
+						</SwiperSlide>
+						<SwiperSlide>
+							{" "}
+							<div className={style.textWrapper}>
+								<p className={style.text}>
+									Увійшли до книги рекордів України, як організація, яка
+									допомогла відзначити державними нагородами за один захід
+									найбільшу кількість матерів загиблих військових (350 батьків
+									Героїв отримали відзнаку «Знак пошани» та іменну «Подяку»,і
+									100 матерів медалі «Честь.Слава.Держава» )
+								</p>
+							</div>
+						</SwiperSlide>
+						<SwiperSlide>
+							{" "}
+							<div className={style.textWrapper}>
+								<p className={style.text}>
+									Увійшли до книги рекордів України, як організація, яка
+									допомогла відзначити державними нагородами за один захід
+									найбільшу кількість матерів загиблих військових (350 батьків
+									Героїв отримали відзнаку «Знак пошани» та іменну «Подяку»,і
+									100 матерів медалі «Честь.Слава.Держава» )
+								</p>
+							</div>
+						</SwiperSlide>
+						<SwiperSlide>
+							{" "}
+							<div className={style.textWrapper}>
+								<p className={style.text}>
+									Увійшли до книги рекордів України, як організація, яка
+									допомогла відзначити державними нагородами за один захід
+									найбільшу кількість матерів загиблих військових (350 батьків
+									Героїв отримали відзнаку «Знак пошани» та іменну «Подяку»,і
+									100 матерів медалі «Честь.Слава.Держава» )
+								</p>
+							</div>
+						</SwiperSlide>
+					</Swiper>
+				</>
+			</div>
+		</Container>
+	);
+};
+
+export default OurAchievements;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ export const Home = () => {
 	return (
 		<>
 			<Hero />
-			{/* <OurAchievements /> */}
+			<OurAchievements />
 		</>
 	);
 };
