0f4e1da015e6bcab494fd49250bf43716c403c91
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-09-11 14:24:08 +0300

    mobile story

--- src/modules/Home/Stories/Stories.jsx
+++ src/modules/Home/Stories/Stories.jsx
@@ -2,37 +2,38 @@ import React from "react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "../../../index.scss";
+import "swiper/css/navigation";
 import style from "./Stories.module.scss";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
 import { Container } from "../../shared/Container/Container";
-import sprite from "../../../assets/svg/sprite.svg"
+import sprite from "../../../assets/svg/sprite.svg";
 const Stories = () => {
-    const breakpoints = {
-        768: {
-            slidesPerView: 2,
-            slidesPerGroup: 2,
-            spaceBetween: 24,
-        },
-    }
+	const breakpoints = {
+		768: {
+			slidesPerView: 2,
+			slidesPerGroup: 2,
+			spaceBetween: 24,
+		},
+	};
 	return (
 		<Container>
 			<section className={style.section}>
 				<h2 className={style.title}>Історії, які надихають</h2>
 				<div className={style.swiperMobile}>
-					<Swiper
-						navigation={{
-							nextEl: ".swiper-button-next1",
-							prevEl: ".swiper-button-prev2",
-						}}
-						pagination={true}
-						breakpoints={breakpoints}
-						modules={[Pagination, Navigation]}
-						className='myStorySwiper'>
-						<SwiperSlide>
-							{" "}
-							<div className={style.paginationWrapper}>
-								<div className={style.contentWrapper}>
+					<div className='mainWrapperForStorySlider'>
+						<Swiper
+							navigation={{
+								nextEl: ".swiper-button-next1",
+								prevEl: ".swiper-button-prev1",
+							}}
+							pagination={true}
+							modules={[Pagination, Navigation]}
+							className='myStorySwiper'
+							breakpoints={breakpoints}>
+							<SwiperSlide>
+								{" "}
+								<div className={style.paginationWrapper}>
 									<div className={style.photo}></div>
 									<div className={style.storyWrapper}>
 										<h3 className={style.storyTitle}>
@@ -52,11 +53,9 @@ const Stories = () => {
 										</div>
 									</div>
 								</div>
-							</div>
-						</SwiperSlide>
-						<SwiperSlide>
-							<div className={style.paginationWrapper}>
-								<div className={style.contentWrapper}>
+							</SwiperSlide>
+							<SwiperSlide>
+								<div className={style.paginationWrapper}>
 									<div className={style.photo}></div>
 									<div className={style.storyWrapper}>
 										<h3 className={style.storyTitle}>
@@ -76,11 +75,9 @@ const Stories = () => {
 										</div>
 									</div>
 								</div>
-							</div>
-						</SwiperSlide>
-						<SwiperSlide>
-							<div className={style.paginationWrapper}>
-								<div className={style.contentWrapper}>
+							</SwiperSlide>
+							<SwiperSlide>
+								<div className={style.paginationWrapper}>
 									<div className={style.photo}></div>
 									<div className={style.storyWrapper}>
 										<h3 className={style.storyTitle}>
@@ -100,9 +97,63 @@ const Stories = () => {
 										</div>
 									</div>
 								</div>
-							</div>
-						</SwiperSlide>
-					</Swiper>
+							</SwiperSlide>
+							<SwiperSlide>
+								<div className={style.paginationWrapper}>
+									<div className={style.photo}></div>
+									<div className={style.storyWrapper}>
+										<h3 className={style.storyTitle}>
+											Малі дії, Великі зміни{" "}
+										</h3>
+										<p className={style.storyText}>
+											Деякі невеликі дії можуть породити неймовірні зміни, і ця
+											історія показує, як спільнота з'єдналася для створення
+											позитивного впливу на світ навколо.
+										</p>
+
+										<div className={style.iconWrapper}>
+											<svg className={style.clockIcon}>
+												<use xlinkHref={`${sprite}#icon-time`}></use>
+											</svg>
+											<span>5 хвилин</span>
+										</div>
+									</div>
+								</div>
+							</SwiperSlide>
+							<SwiperSlide>
+								<div className={style.paginationWrapper}>
+									<div className={style.photo}></div>
+									<div className={style.storyWrapper}>
+										<h3 className={style.storyTitle}>
+											Малі дії, Великі зміни{" "}
+										</h3>
+										<p className={style.storyText}>
+											Деякі невеликі дії можуть породити неймовірні зміни, і ця
+											історія показує, як спільнота з'єдналася для створення
+											позитивного впливу на світ навколо.
+										</p>
+
+										<div className={style.iconWrapper}>
+											<svg className={style.clockIcon}>
+												<use xlinkHref={`${sprite}#icon-time`}></use>
+											</svg>
+											<span>5 хвилин</span>
+										</div>
+									</div>
+								</div>
+							</SwiperSlide>
+						</Swiper>
+						<div className='swiper-button-next1'>
+							<svg>
+								<use xlinkHref={`${sprite}#icon-right`}></use>
+							</svg>
+						</div>
+						<div className='swiper-button-prev1'>
+							<svg>
+								<use xlinkHref={`${sprite}#icon-left`}></use>
+							</svg>
+						</div>
+					</div>
 				</div>
 			</section>
 		</Container>
