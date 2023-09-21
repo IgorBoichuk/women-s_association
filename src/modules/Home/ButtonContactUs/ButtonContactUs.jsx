ed56bde7dcac73f23954a5fa25e899f1d7133ff2
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-09-21 23:43:32 +0300

    Invitation + ButtonMain + ButtonContactUs

--- src/modules/Home/ButtonContactUs/ButtonContactUs.jsx
+++ src/modules/Home/ButtonContactUs/ButtonContactUs.jsx
@@ -1,20 +1,14 @@
-import React from 'react'
-import style from './ButtonContactUs.module.scss'
-import sprite from '../../../assets/svg/sprite.svg'
+import React from "react";
+import style from "./ButtonContactUs.module.scss";
+import sprite from "../../../assets/svg/sprite.svg";
 const ButtonContactUs = ({ onClick }) => {
-	return (
-		<button
-			type='button'
-			onClick={onClick}
-			className={style.buttonContactUs}>
-			<svg
-				width='40px'
-				height='40px'
-				className=''>
-				<use xlinkHref={`${sprite}#icon-mdi_email-edit-outline`}></use>
-			</svg>
-		</button>
-	);
+  return (
+    <button type="button" onClick={onClick} className={style.buttonContactUs}>
+      <svg width="30px" height="30px" className="">
+        <use xlinkHref={`${sprite}#icon-mdi_email-edit-outline`}></use>
+      </svg>
+    </button>
+  );
 };
 
-export default ButtonContactUs
+export default ButtonContactUs;
--- src/modules/Home/Invitation/Invitation.jsx
+++ src/modules/Home/Invitation/Invitation.jsx
@@ -3,7 +3,6 @@ import style from "./Invitation.module.scss";
 import ButtonMain from "../../shared/ButtonMain/ButtonMain";
 import { Container } from "../../shared/Container/Container";
 import { useGetInvitationContent } from "../../../hooks/home/invitations/useGetInvitationContent";
-import bgPicture from "../../../assets/img/Invitation/lediesmob.jpg";
 
 export const Invitation = () => {
   const { invitation, loadingInvitation } = useGetInvitationContent();
@@ -14,15 +13,10 @@ export const Invitation = () => {
         {invitation && !loadingInvitation && (
           <div className={style.wrapper}>
             <h3 className={style.title}>{invitation.title}</h3>
-            <div className={style.imgWrapper}>
-              <img
-                src={bgPicture}
-                alt="Жінки та матері військовослужбовців, групове фото"
-                className={style.bgImg}
-              />
+            <div className={style.descriptionWrapper}>
+              <p className={style.description}>{invitation.invitation}</p>
+              <ButtonMain>{invitation.button}</ButtonMain>
             </div>
-            <p className={style.description}>{invitation.invitation}</p>
-            <ButtonMain>{invitation.button}</ButtonMain>
           </div>
         )}
       </Container>
